25cf18ebcd7e38563ab73830e22ec559e67482e4
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2022-10-25 11:18:27 +0200

    Fix merge (#8)
    * Add output panel and write to it when button is clicked
    
    * pr fixes
    
    * Minor updates

---
+++ SimpleVSIX/SimpleVSIX/CommandOutputWindow.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.RpcContracts.Commands;
+using Task = System.Threading.Tasks.Task;
+
+namespace SimpleVSIX
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CommandOutputWindow
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("7a9fcb09-91b7-4b18-acf2-7a320e74d4ff");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+
+        private IVsOutputWindowPane pane;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandOutputWindow"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CommandOutputWindow(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CommandOutputWindow Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in Command1's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new CommandOutputWindow(package, commandService);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            EnsurePaneExists();
+            UpdatePane();
+        }
+
+        private void EnsurePaneExists()
+        {
+            if (pane != null) return;
+            var output = package.GetService<SVsOutputWindow, IVsOutputWindow>();
+            var guid = Guid.NewGuid();
+
+            // Create a new pane.
+            output.CreatePane(ref guid, "Step 5", Convert.ToInt32(true), Convert.ToInt32(false));
+
+            // Retrieve the new pane.
+            output.GetPane(ref guid, out pane);
+        }
+
+        private void UpdatePane()
+        {
+            pane.OutputStringThreadSafe("Hello!!\n");
+        }
+    }
+}
