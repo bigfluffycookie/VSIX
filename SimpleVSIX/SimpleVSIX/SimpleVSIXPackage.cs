ebf2b797acfcf0e7cf00565b6fe92dd015ce39e1
Leyla <54935347+bigfluffycookie@users.noreply.github.com>
2022-10-25 10:53:34 +0200

    Revert "Add output panel and write to it when button is clicked (#5)"
    This reverts commit 19fd38ab31778df306c0e623a205e2dd1148ee15.

--- SimpleVSIX/SimpleVSIX/SimpleVSIXPackage.cs
+++ SimpleVSIX/SimpleVSIX/SimpleVSIXPackage.cs
@@ -25,7 +25,6 @@ namespace SimpleVSIX
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(SimpleVSIXPackage.PackageGuidString)]
-    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class SimpleVSIXPackage : AsyncPackage
     {
         /// <summary>
@@ -47,7 +46,6 @@ namespace SimpleVSIX
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            await CommandOutputWindow.InitializeAsync(this);
         }
 
         #endregion
